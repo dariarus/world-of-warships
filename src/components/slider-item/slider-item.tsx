92ff8e271038acdd86e6f3b6a1c11a3f56a53ca9
dariarus <rusanova_dv@mail.ru>
2023-12-11 00:49:52 +0300

    feature: переделать кнопку Show more - теперь она просто находится в списке, а не закрывает собой последний элемент. Пагинация по 24 элемента, а при загрузке видно 23. Изменения стилей кнопки Show more

--- src/components/slider-item/slider-item.tsx
+++ src/components/slider-item/slider-item.tsx
@@ -6,17 +6,13 @@ import sliderItemStyles from './slider-item.module.css';
 import mainStore from '../../stores';
 import {SliderItemStore} from '../../stores/slider-item-store';
 
-import {visibleItems} from '../../utils/constants';
-
 import {SliderItemActivator} from '../../types/data';
 
 export const SliderItem: FunctionComponent<{
   sliderItemStore: SliderItemStore,
-  isShowMoreButton?: boolean
   // index: number
 }> = observer(({
                  sliderItemStore,
-                 isShowMoreButton,
                  // index
                }) => {
   const handleOnClickOnItem = () => {
@@ -25,56 +21,37 @@ export const SliderItem: FunctionComponent<{
     mainStore.fullWarshipsListStore.setListIsClose();
   }
 
-  const handleOnShowMoreItems = () => {
-    mainStore.filtersDataStore.setVisibleItems(visibleItems);
-  }
-
   return (
-    <>
-      {
-        isShowMoreButton
-          ? <button
-            className={`${sliderItemStyles.item} ${sliderItemStyles.item_more}`}
-            onClick={handleOnShowMoreItems}
-          >
-            Show more
-          </button>
-          : <>
-            {
-              sliderItemStore &&
-              <button
-                className={
-                  sliderItemStore.isActive
-                    ? `${sliderItemStyles.item} ${sliderItemStyles.item_info} ${sliderItemStyles.item_active}`
-                    : `${sliderItemStyles.item} ${sliderItemStyles.item_info}`}
-                style={{
-                  backgroundImage: `linear-gradient(rgba(36, 36, 36, .5), rgba(36, 36, 36, .5)), url(${(sliderItemStore.warship.nation.icons?.large || '')})`
-                }}
-                onClick={handleOnClickOnItem}
-              >
-                <div className={sliderItemStyles['item__type-level-container']}>
-                  <img
-                    src={sliderItemStore.warship.type.icons.default}
-                    alt="Warship's type icon"
-                    className={`${sliderItemStyles.image} ${sliderItemStyles['image_warship-type']}`}
-                  />
-                  <p className={sliderItemStyles.text}>{sliderItemStore.warship.level}</p>
-                </div>
-                <img
-                  src={sliderItemStore.warship.icons.medium}
-                  alt="Warship's image"
-                  className={`${sliderItemStyles.image} ${sliderItemStyles.image_warship}`}
-                />
-                <div className={sliderItemStyles['item__title-nation-container']}>
-                  {/*<p>{index}</p>*/}
-                  <p className={sliderItemStyles.text}>{sliderItemStore.warship.nation.title}</p>
-                  <p
-                    className={`${sliderItemStyles.text} ${sliderItemStyles['text_upper-case']}`}>{sliderItemStore.warship.title}</p>
-                </div>
-              </button>
-            }
-          </>
-      }
-    </>
+    sliderItemStore &&
+    <button
+      className={
+        sliderItemStore.isActive
+          ? `${sliderItemStyles.item} ${sliderItemStyles.item_info} ${sliderItemStyles.item_active}`
+          : `${sliderItemStyles.item} ${sliderItemStyles.item_info}`}
+      style={{
+        backgroundImage: `linear-gradient(rgba(36, 36, 36, .5), rgba(36, 36, 36, .5)), url(${(sliderItemStore.warship.nation.icons?.large || '')})`
+      }}
+      onClick={handleOnClickOnItem}
+    >
+      <div className={sliderItemStyles['item__type-level-container']}>
+        <img
+          src={sliderItemStore.warship.type.icons.default}
+          alt="Warship's type icon"
+          className={`${sliderItemStyles.image} ${sliderItemStyles['image_warship-type']}`}
+        />
+        <p className={sliderItemStyles.text}>{sliderItemStore.warship.level}</p>
+      </div>
+      <img
+        src={sliderItemStore.warship.icons.medium}
+        alt="Warship's image"
+        className={`${sliderItemStyles.image} ${sliderItemStyles.image_warship}`}
+      />
+      <div className={sliderItemStyles['item__title-nation-container']}>
+        {/*<p>{index}</p>*/}
+        <p className={sliderItemStyles.text}>{sliderItemStore.warship.nation.title}</p>
+        <p
+          className={`${sliderItemStyles.text} ${sliderItemStyles['text_upper-case']}`}>{sliderItemStore.warship.title}</p>
+      </div>
+    </button>
   )
 })
