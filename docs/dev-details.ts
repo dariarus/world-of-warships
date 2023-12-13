9dfd3c9e2486766ddb62469310f17a7da24d23a0
dariarus <rusanova_dv@mail.ru>
2023-12-13 22:08:08 +0300

    добавить ссылку на задание в docs

--- docs/dev-details.ts
+++ docs/dev-details.ts
@@ -5,3 +5,5 @@
 -Разработка в WebStorm на Windows
 Для работы сделать то же самое: удалить node_modules и package-lock и установить их через терминал в WebStorm.
 Тогда будут доступны запуск по команде dev из package.json и отображение изменений в реальном времени в браузере. */
+
+/* Тестовое задание: https://gist.github.com/nonamenix/fc7609de3ebe2642db6324bc962295b2 */
